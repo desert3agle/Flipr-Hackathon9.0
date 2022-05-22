033072f557e232570b726c7a14705b8391293877
Harsh Vardhan <harsh.vardhan320@gmail.com>
2022-05-22 18:43:08 +0530

    lets see

--- config/passport.js
+++ config/passport.js
@@ -33,7 +33,7 @@ module.exports = function (passport) {
     passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: '/auth/google/callback',
+        callbackURL: 'https://zapmail-flipr-hackathon.herokuapp.com/auth/google/callback',
     },
         async (accessToken, refreshToken, profile, done) => {
             const newUser = {
